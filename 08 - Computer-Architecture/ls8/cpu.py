e01bee7e94d4b19a35669a6e46cb81d8ab723478
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2020-12-30 22:51:56 -0500

    Updated after evening class, need to finish.

--- 08 - Computer-Architecture/ls8/cpu.py
+++ 08 - Computer-Architecture/ls8/cpu.py
@@ -17,23 +17,23 @@ class CPU:
 
         HLT = 0b00000001   # Halt - instruction handler - stops the program/computer
         LDI = 0b10000011   # Load Immediately - instruction -
-        MUL = 0b10100010   # Print - Instruction -
-        PRN = 0b01000111
-        POP = 0b01000110
-        PUSH = 0b01000101
-        RET = 0b00010001
-        CALL = 0b01010000
+        MUL = 0b10100010
+        PRN = 0b01000111   # Print - Instruction -
+        POP = 0b01000110   # Pop - removes value(s)
+        PUSH = 0b01000101  # Push - adds value(s)
+        RET = 0b00010001   # Return -
+        CALL = 0b01010000  # Call -
 
         self.IR = {
-            HLT =  self.HLT,   # Halt - Stops the program/computer
-            LDI =  self.LDI,   # Load Immediately - instruction -
-            MUL =  self.MUL,   #
-            PRN =  self.PRN,   # Print - Instruction -
-            POP =  self.POP,   # Pop - Removes most recent value from the Stack
-            PUSH = self.PUSH,  # Push - adds to Stack
-            RET =  self.RET,   # Return -
-            CALL = self.CALL   # Call -
-        }
+                HLT =  self.HLT,   # Halt - Stops the program/computer
+                LDI =  self.LDI,   # Load Immediately - instruction -
+                MUL = self.MUL,  #
+                PRN = self.PRN,  # Print - Instruction -
+                POP = self.POP,  # Pop - Removes most recent value from the Stack
+                PUSH = self.PUSH,  # Push - adds to Stack
+                RET = self.RET,  # Return -
+                CALL = self.CALL  # Call -
+       }
 
 
     def HLT(self):  # Halt - stops the program/computer
@@ -110,6 +110,100 @@ class CPU:
 
         print()
 
+    def PC_move(self, operation):
+        move = operation & 0b1000000
+        move_num = (move >> 6) + 1
+        return move_num
+
+
     def run(self):
         """Run the CPU."""
-        pass
+        operation = self.memory
+        PC = self.pc
+
+        while self.stopped == False:
+            op = operation[PC]
+
+            if op == 0b10000010:
+                self.reg[operation[PC + 1]] = operation[PC + 2]
+                PC += self.PC_move(op)
+
+            elif op == 0b10100000:
+                self.alu("ADD", self.memory[PC + 1], self.memory[PC + 2])
+                PC += self.PC_move(op)
+
+            elif op == 0b00001000:
+                PC += self.PC_move(op)
+
+            elif op == 0b10100010:
+                self.alu("MULTI", self.memory[PC + 1], self.memory[PC + 2])
+                PC += self.PC_move(op)
+
+            elif op == 0b01000111:
+                print(self.reg[operation[PC + 1]])
+                PC += self.PC_move(op)
+
+            elif op == 0b00000001:
+                self.stopped = True
+                self.HLT()
+                PC += self.PC_move(op)
+
+            elif op == 0b01000101:  # PUSH
+                self.SP -= 1
+                reg_loc = self.memory[PC + 1]
+                self.memory[self.SP] = self.reg[reg_loc]
+                self.reg[7] = self.SP
+                PC += self.PC_move(op)
+
+            elif op == 0b01000110:  # POP
+                value = self.memory[self.reg[7]]
+                reg_loc = self.memory[PC + 1]
+                self.SP += 1
+                self.reg[7] = self.SP
+                self.reg[reg_loc] = value
+                PC += self.PC_move(op)
+
+            elif op == 0b01010000:  # CALL
+                self.SP -= 1
+                self.memory[self.SP] = PC + 2
+                PC = self.reg[self.memory[PC + 1]]
+
+            elif op == 0b00010001:  # RETURN
+                PC = self.memory[self.SP]
+                self.SP += 1
+
+            elif op == 0b10100111:  # CMP
+                self.FLG(self.memory[PC + 1], self.mempry[PC + 2])
+                PC += self.PC_move(op)
+
+            elif op == 0b01100110:  # DEC
+                self.reg[PC + 1] -= 1
+                PC += self.PC_move(op)
+
+            elif op == 0b10100011:  # DIV
+                self.alu("DIV", self.memory[PC + 1], self.memory[PC + 2])
+                PC += self.PC_move(op)
+
+            elif op == 0b01100101:  # INCR
+                self.reg[self.memory[PC + 1]] += 1
+                PC += self.PC_move(op)
+
+            elif op == 0b01010101:  # JEQ
+                if self.flag == 0b00000001:
+                    PC = self.reg[self.memory[PC + 1]]
+                else:
+                    PC += self.PC_move(op)
+
+            elif op == 0b01010110:  # JNE
+                if self.flag != 0b00000001:
+                    PC = self.reg[self.memory[PC + 1]]
+                else:
+                    PC += self.PC_move(op)
+
+            elif op == 0b01010100:
+                PC = self.reg[self.memory[PC + 1]]
+
+            else:
+                self.stopped = True
+                print("Error: I am not a teapot")
+                seld.HLT()
